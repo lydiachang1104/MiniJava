50da64ba275631f4d9d203f4260eaaf79edc9a10
lydiachang1104 <705310897@qq.com>
2017-12-31 07:55:31 -0800

    modify the way to print tree

--- src/main.java
+++ src/main.java
@@ -5,23 +5,47 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import java.io.*;
 
 
+//public class main {
+//    public static void main(String[] args) {
+//        ANTLRInputStream antlrstream;
+//
+//        try {
+//            InputStream in = new FileInputStream("factorial.expr");
+//
+//            antlrstream = new ANTLRInputStream(in);
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//            return;
+//        }
+//
+//        MiniJavaLexer lexer = new MiniJavaLexer(antlrstream);
+//        CommonTokenStream tokens = new CommonTokenStream(lexer);
+//        MiniJavaParser parser = new MiniJavaParser(tokens);
+//        String outstr = new tree().print(parser.goal().getRuleContext());
+//    }
+//
+//}
+
 public class main {
-    public static void main(String[] args) {
-        ANTLRInputStream antlrstream;
+    public static void main(String[] args)throws Exception {
 
-        try {
-            InputStream in = new FileInputStream("factorial.expr");
+        InputStream in = new FileInputStream("factorial.expr");
 
-            antlrstream = new ANTLRInputStream(in);
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-            return;
-        }
 
-        MiniJavaLexer lexer = new MiniJavaLexer(antlrstream);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        MiniJavaParser parser = new MiniJavaParser(tokens);
-        String outstr = new tree().print(parser.goal().getRuleContext());
+        ANTLRInputStream input=new ANTLRInputStream(in);
+        MiniJavaLexer lexer=new MiniJavaLexer(input);
+        CommonTokenStream tokens=new CommonTokenStream(lexer);
+        MiniJavaParser parser=new MiniJavaParser(tokens);
+        ParseTree tree=parser.goal();
+
+        tree eval=new tree();
+        eval.visit(tree);
+
+        //print AST string
+        System.out.println(tree.toStringTree(parser));
+
+        //show GUI tree;
+        org.antlr.v4.gui.Trees.inspect(tree,parser);
     }
 }
--- src/tree.java
+++ src/tree.java
@@ -1,64 +1,76 @@
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.tree.ParseTree;
+//import org.antlr.v4.runtime.ANTLRInputStream;
+//import org.antlr.v4.runtime.tree.ParseTree;
+//
+//import org.antlr.v4.runtime.RuleContext;
+//import org.antlr.v4.runtime.tree.TerminalNode;
+//import org.antlr.v4.runtime.CommonTokenStream;
+//
+//import java.io.*;
+//
+//
+//public class tree {
+//    private StringBuffer buffer;
+//    public String print(RuleContext ctx) {
+//        buffer = new StringBuffer();
+//        explore(ctx, 0);
+//        return buffer.toString();
+//    }
+//
+//    private void explore(RuleContext ctx, int indentation) {
+//        String ruleName = MiniJavaParser.ruleNames[ctx.getRuleIndex()];
+//        print_Indentation(indentation);
+//        System.out.println(ruleName);
+//        buffer.append(ruleName).append('\n');
+//        for (int i=0;i<ctx.getChildCount();i++) {
+//            ParseTree element = ctx.getChild(i);
+//            if (element instanceof RuleContext) {
+//                explore((RuleContext)element, indentation + 1);
+//
+//            } else if (element instanceof TerminalNode) {
+//                explore_Terminal((TerminalNode) element, indentation + 1);
+//            }
+//        }
+//    }
+//
+//    private void explore_Terminal(TerminalNode element, int indentation) {
+//        int type = element.getSymbol().getType();
+//        switch (type) {
+//            case MiniJavaParser.
+//            case MiniJavaParser.ID:
+//                print_Indentation(indentation);
+//                buffer.append("id " + element.getText()).append('\n');
+//                System.out.println("id " + element.getText()); break;
+//            case MiniJavaParser.BOOL:
+//                print_Indentation(indentation);
+//                buffer.append("bool " + element.getText()).append('\n');
+//                System.out.println("bool " + element.getText()); break;
+//            case MiniJavaParser.INT:
+//                print_Indentation(indentation);
+//                buffer.append("int " + element.getText()).append('\n');
+//                System.out.println("int " + element.getText()); break;
+//        }
+//    }
+//
+//    private void print_Indentation(int indentation) {
+//        if (indentation > 0) {
+//            for (int i = 0; i < indentation - 1; i++) {
+//                buffer.append("|   ");
+//                System.out.print("|   ");
+//            }
+//            buffer.append("|..");
+//            System.out.print("|..");
+//        }
+//
+//    }
+//}
 
-import org.antlr.v4.runtime.RuleContext;
-import org.antlr.v4.runtime.tree.TerminalNode;
-import org.antlr.v4.runtime.CommonTokenStream;
-
-import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
-public class tree {
-    private StringBuffer buffer;
-    public String print(RuleContext ctx) {
-        buffer = new StringBuffer();
-        explore(ctx, 0);
-        return buffer.toString();
-    }
-
-    private void explore(RuleContext ctx, int indentation) {
-        String ruleName = MiniJavaParser.ruleNames[ctx.getRuleIndex()];
-        print_Indentation(indentation);
-        System.out.println(ruleName);
-        buffer.append(ruleName).append('\n');
-        for (int i=0;i<ctx.getChildCount();i++) {
-            ParseTree element = ctx.getChild(i);
-            if (element instanceof RuleContext) {
-                explore((RuleContext)element, indentation + 1);
-
-            } else if (element instanceof TerminalNode) {
-                explore_Terminal((TerminalNode) element, indentation + 1);
-            }
-        }
-    }
-
-    private void explore_Terminal(TerminalNode element, int indentation) {
-        int type = element.getSymbol().getType();
-        switch (type) {
-            case MiniJavaParser.ID:
-                print_Indentation(indentation);
-                buffer.append("id " + element.getText()).append('\n');
-                System.out.println("id " + element.getText()); break;
-            case MiniJavaParser.BOOL:
-                print_Indentation(indentation);
-                buffer.append("bool " + element.getText()).append('\n');
-                System.out.println("bool " + element.getText()); break;
-            case MiniJavaParser.INT:
-                print_Indentation(indentation);
-                buffer.append("int " + element.getText()).append('\n');
-                System.out.println("int " + element.getText()); break;
-        }
-    }
-
-    private void print_Indentation(int indentation) {
-        if (indentation > 0) {
-            for (int i = 0; i < indentation - 1; i++) {
-                buffer.append("|   ");
-                System.out.print("|   ");
-            }
-            buffer.append("|..");
-            System.out.print("|..");
-        }
-
-    }
+public class tree extends MiniJavaBaseVisitor<Integer> {
+    Map<String,Integer> memory=new HashMap<String,Integer>();
+//    public  Integer visitExpCalc(MiniJavaParser.ExpCalcContext ctx){
+//        return 0;
+//    }
 }
